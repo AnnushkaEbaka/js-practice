4d97115812877b32b33ba2683ce61da643004627
Anna <mordovchenko1009@gmail.com>
2024-12-15 19:11:40 +0400

    синхронизация локальных файлов с гитом

---
+++ js-practice/types.js
@@ -0,0 +1,21 @@
+let string = 'Пицца';
+console.log (string)
+
+
+let booleen = true;
+
+
+let name = null;
+console.log (name)
+
+let name;
+alert (name)
+
+
+
+let number = 234;
+alert (number)
+
+
+const bigInt = 234567890123456789n;
+console.log (bigInt)
